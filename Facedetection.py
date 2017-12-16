85a8dd663bf5e6df67b6ced1a138913eea8a4c21
Nikolaj Holden <nikolajholden@gmail.com>
2017-12-16 12:26:57 +0100

    continuous

--- Facedetection.py
+++ Facedetection.py
@@ -65,7 +65,7 @@ for foo in camera.capture_continuous(highResCap, format="bgr", use_video_port=Tr
         #     cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
     servo_val=np.clip(servo_val+errorX,min_servo_val,max_servo_val)
-    call(["echo 2="+servo_val+" > /dev/servoblaster"],shell=False)
+    call(["echo 2="+str(servo_val)+" > /dev/servoblaster"],shell=False)
     print(time.clock()-start)
     #cv2.imshow('img',img)
     #cv2.waitKey(1)
