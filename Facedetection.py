b116d8a2a40d65ceacd27fe88f5f06cd8d0f6d06
Nikolaj Holden <nikolajholden@gmail.com>
2017-12-16 14:33:27 +0100

    continuous

--- Facedetection.py
+++ Facedetection.py
@@ -35,6 +35,10 @@ highResCap = PiRGBArray(camera)
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
+roi_gray=None
+width=None
+height=None
+
 for foo in camera.capture_continuous(highResCap, format="bgr", use_video_port=True):
     start=time.clock()
     stream = io.BytesIO()
@@ -55,14 +59,26 @@ for foo in camera.capture_continuous(highResCap, format="bgr", use_video_port=Tr
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     #print("Before finding faces")
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    if len(faces)==0:
+        if roi_gray != None:
+            print("template matching")
+            # Apply template Matching
+            res = cv2.matchTemplate(img, roi_gray, cv2.TM_CCOEFF)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+            top_left=max_loc
+            faces=[[top_left[0],top_left[1],top_left[0] + width, top_left[1] + height]]
+
+
     errorX=0
     #print("num faces: ",len(faces))
     for (x,y,w,h) in faces:
         # cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        # roi_gray = gray[y:y+h, x:x+w]
+        roi_gray = gray[y:y+h, x:x+w]
+        width=w
+        heigth=g
         # roi_color = gray[y:y+h, x:x+w]
         center=[x+(w/2),y+(h/2)]
-        #print("Center",center)
+        print("Center",center)
         errorX=(resX/2)-center[0] # we want to center on x-axis
         #print("Error x",errorX)
 
