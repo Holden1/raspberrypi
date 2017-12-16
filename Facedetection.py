0fbf37aa2ba09b18af4353fb61a725b9adeaaf89
Nikolaj Holden <nikolajholden@gmail.com>
2017-12-16 11:22:51 +0100

    next

--- Facedetection.py
+++ Facedetection.py
@@ -26,22 +26,13 @@ eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 while True:
     print("In loop")
     start=time.clock()
-
     stream = io.BytesIO()
     # capture into stream
-    print("Capturing")
     camera.capture(stream, format='jpeg', use_video_port=True)
     # convert image into numpy array
-    print("Convert to data")
     data = np.fromstring(stream.getvalue(), dtype=np.uint8)
     # turn the array into a cv2 image
-    print("convert to img")
     img = cv2.imdecode(data, 1)
-    #cv2.imshow("preview", frame)
-    # hrs = highResStream.__next__()
-    # img=hrs.array
-    # highResStream.truncate(0)
-    #img = cv2.imread('image.jpg')
 
     print("after to img")
     r = 1000.0 / img.shape[1]
@@ -53,6 +44,7 @@ while True:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     print("Before finding faces")
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    print("num faces: ",len(faces))
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
@@ -61,6 +53,6 @@ while True:
         # for (ex,ey,ew,eh) in eyes:
         #     cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
     print(time.clock()-start)
-    cv2.imshow('img',img)
-    cv2.waitKey(1)
+    #cv2.imshow('img',img)
+    #cv2.waitKey(1)
     #cv2.destroyAllWindows()
